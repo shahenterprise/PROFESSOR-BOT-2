b8136cc7a7ac175b5619516bc07fdf084122b15f
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2023-09-24 09:30:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-import re, time
+import re, time, os
 from os import environ
 from Script import script 
 
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6426079137:AAGrLCet1GFVZLHIlZaHwA1gnYJMabA
 
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/6bb2f708437debb800c24.jpgh ttps://graph.org/file/288acba07641008f745e9.jpg https://graph.org/file/97b2e670c1190bf403d47.jpg https://graph.org/file/0ca956cbe497525970aef.jpg https://graph.org/file/95b1fc0d0f5e2af2721c9.jpg https://graph.org/file/a5ac821b5e6ea15fa51c3.jpg https://graph.org/file/0795a9c7b9780fac5e9b8.jpg https://graph.org/file/74437db6be090e34613fb.jpg https://graph.org/file/5c72c6b82af7bcb6bb07e.jpg https://graph.org/file/5c72c6b82af7bcb6bb07e.jpg')).split()
+PICS = (environ.get('PICS' ,'https://graph.org/file/6bb2f708437debb800c24.jpg https://graph.org/file/288acba07641008f745e9.jpg https://graph.org/file/97b2e670c1190bf403d47.jpg https://graph.org/file/0ca956cbe497525970aef.jpg https://graph.org/file/95b1fc0d0f5e2af2721c9.jpg https://graph.org/file/a5ac821b5e6ea15fa51c3.jpg https://graph.org/file/0795a9c7b9780fac5e9b8.jpg https://graph.org/file/74437db6be090e34613fb.jpg https://graph.org/file/5c72c6b82af7bcb6bb07e.jpg https://graph.org/file/5c72c6b82af7bcb6bb07e.jpg')).split()
 UPTIME = time.time()
 
 # Admins, Channels & Users
@@ -44,7 +44,7 @@ START_MESSAGE = environ.get('START_MESSAGE', script.START_TXT)
 BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", script.BUTTON_LOCK_TEXT)
 FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', script.FORCE_SUB_TEXT)
 
-WELCOM_PIC = environ.get("WELCOM_PIC", "")
+WELCOM_PIC = environ.get("WELCOM_PIC", "https://graph.org/file/6bb2f708437debb800c24.jpg")
 WELCOM_TEXT = environ.get("WELCOM_TEXT", script.WELCOM_TEXT)
 PMFILTER = is_enabled(environ.get('PMFILTER', "True"), True)
 G_FILTER = is_enabled(environ.get("G_FILTER", "True"), True)
@@ -52,7 +52,7 @@ BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
 RemoveBG_API = environ.get("RemoveBG_API", "")
 
 # url shortner
-SHORT_URL = environ.get("SHORT_URL", 'onepagelink.in')
+SHORT_URL = environ.get("SHORT_URL", 'https://onepagelink.in')
 SHORT_API = environ.get("SHORT_API", 'e6ec7e087b9a81081a8fa7791a9df7973b51b9e1')
 
 # Others
@@ -63,7 +63,7 @@ P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
 PM_IMDB = is_enabled(environ.get('PM_IMDB', "False"), False)
 IMDB = is_enabled(environ.get('IMDB', "False"), False)
 SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', "True"), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_name}")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "▪️ 𝐅𝐢𝐥𝐞 𝐧𝐚𝐦𝐞 : {file_name}\n▪️ 𝐅𝐢𝐥𝐞 𝐒𝐢𝐳𝐞 : {file_size}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", None)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
@@ -75,7 +75,13 @@ PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
 PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
 LOG_MSG = "{} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ....✨\n\n🗓️ Dᴀᴛᴇ : {}\n⏰ Tɪᴍᴇ : {}\n\n🖥️ Rᴇᴏᴩ: {}\n🉐 Vᴇʀsɪᴏɴ: {}\n🧾 Lɪᴄᴇɴꜱᴇ: {}\n©️ Cᴏᴩʏʀɪɢʜᴛ: {}"
 
+BLACKLIST_WORDS = (
+    list(os.environ.get("BLACKLIST_WORDS").split(","))
+    if os.environ.get("BLACKLIST_WORDS")
+    else []
+)
 
+BLACKLIST_WORDS = ["@BM Links", "[BindasMovies]", "[Hezz Movies]", "www Tamilblasters rent", "E4E", "[D&O]", "[MM]", "[", "]", "[FC]", "[CF]", "LinkZz", "[DFBC]", "@New_Movie", "@Infinite_Movies2", "MM", "@R A R B G", "[F&T]", "[KMH]", "[DnO]", "[F&T]", "MLM", "@TM_LMO", "@x265_E4E", "@HEVC MoviesZ", "SSDMovies", "@MM Linkz", "[CC]", "@Mallu_Movies", "@DK Drama", "@luxmv_Linkz", "@Akw_links", "CK HEVC", "@Team_HDT", "[CP]", "www 1TamilMV men", "www TamilRockers", "@MM", "@mm", "[MW]", "@TN68 Linkzz", "@Clipmate_Movie", "[MASHOBUC]", "Official TheMoviesBoss", "www CineVez one", "www 7MovieRulz lv", "www 1TamilMV vip", "[SMM Official]"]
 
 
 
